6dcb5002b867a7cc4784f2b2e9e27a72459f52e9
fatima204 <fatima.amrin.hossain@gmail.com>
2026-03-28 15:12:39 +0100

    base 1st commit

---
+++ CurrencyConverter.Web/CurrencyService.cs
@@ -0,0 +1,40 @@
+﻿namespace CurrencyConverter.Web
+{
+    using System.Xml.Linq;
+
+    public class CurrencyService : ICurrencyService
+    {
+        private const string ECB_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxrefdaily.xml";
+        private const string VALID_KEY = "secret123";
+
+        public double Convert(string fromCurrency, string toCurrency, double amount, string apiKey)
+        {
+            if (apiKey != VALID_KEY)
+            {
+                throw new Exception("Authentication failed");
+            }
+
+            var rates = LoadRates();
+
+            double fromRate = fromCurrency == "EUR" ? 1 : rates[fromCurrency];
+            double toRate = toCurrency == "EUR" ? 1 : rates[toCurrency];
+
+            double eurAmount = amount / fromRate;
+            return eurAmount * toRate;
+        }
+
+        private Dictionary<string, double> LoadRates()
+        {
+            var xml = XDocument.Load(ECB_URL);
+
+            var rates = xml.Descendants()
+                .Where(x => x.Name.LocalName == "Cube" && x.Attribute("currency") != null)
+                .ToDictionary(
+                    x => x.Attribute("currency").Value,
+                    x => double.Parse(x.Attribute("rate").Value)
+                );
+
+            return rates;
+        }
+    }
+}
